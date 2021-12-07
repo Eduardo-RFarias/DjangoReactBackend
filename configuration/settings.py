835ec644dcc4decdac22be1663ccb7147a5dacec
Eduardo Rodrigues de Farias <eduardo.rfarias@outlook.com>
2021-12-07 19:13:41 -0300

    fix pytz bug

--- configuration/settings.py
+++ configuration/settings.py
@@ -42,13 +42,13 @@ ALLOWED_HOSTS = [HEROKU_URL, "localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "corsheaders",
     "apps.Todo",
 ]
